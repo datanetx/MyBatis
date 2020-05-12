ab8c6900f627223cc11f7551751d178fd8bf0921
datanetlw <datanet@126.com>
2020-05-12 14:16:23 +0800

    V1.0.4

--- src/test/java/com/datanetx/test/TestMain.java
+++ src/test/java/com/datanetx/test/TestMain.java
@@ -10,26 +10,31 @@ import com.datanetx.utils.ServiceFactory;
 import com.datanetx.utils.TransactionInvocationHandler;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.List;
 
 public class TestMain {
 
     @Test
     public void mytest() {
-        DeptService deptService=(DeptService) ServiceFactory.getService(new DeptServiceImpl());
-//        deptService.update(new Dept(60,"办公室","西藏"));
 
+        DeptService deptService=(DeptService) ServiceFactory.getService(new DeptServiceImpl());
+        deptService.update(new Dept(60, "办公室", "西藏"));
+
+        deptService = (DeptService) ServiceFactory.getService(new DeptServiceImpl());
         List<Dept> deptList=deptService.selectAll();
         for (Dept dept:deptList) {
             System.out.println(dept);
         }
 
-//        EmpService empService=(EmpService) ServiceFactory.getService(new EmpServiceImpl());
-////        EmpService empService=(EmpService) new TransactionInvocationHandler(new EmpServiceImpl()).getProxy();//也可以不用ServiceFactory
-//        empService.insert(new Emp(9003,"leo","刀客",9000,"1977-07-07",2000,200,50));
-//        List<Emp> empList=empService.selectAll();
-//        for(Emp emp:empList){
-//            System.out.println(emp);
-//        }
+        EmpService empService = (EmpService) ServiceFactory.getService(new EmpServiceImpl());
+//        EmpService empService=(EmpService) new TransactionInvocationHandler(new EmpServiceImpl()).getProxy();//也可以不用ServiceFactory
+        empService.insert(new Emp(9003, "leo", "刀客", 9000, new Date(), 2000.0, 200.0, 50));
+
+        empService = (EmpService) ServiceFactory.getService(new EmpServiceImpl());
+        List<Emp> empList = empService.selectAll();
+        for (Emp emp : empList) {
+            System.out.println(emp);
+        }
     }
 }
